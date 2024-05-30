8b41b50c751579347e18b7699d10b2bb216809f6
ivanmvm <113584496+ivanmvm@users.noreply.github.com>
2024-05-30 11:52:24 +0200

    mini_practica

--- llibrespractica/src/main/java/com/minipractica/DatabaseManager.java
+++ llibrespractica/src/main/java/com/minipractica/DatabaseManager.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class DatabaseManager {
     private static final String URL = "jdbc:mysql://localhost:3306/library";
     private static final String USER = "root";
-    private static final String PASSWORD = "yourpassword";
+    private static final String PASSWORD = "pirineus";
     private Connection connection;
 
     public DatabaseManager() throws SQLException {
